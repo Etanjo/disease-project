4a97005adc69bbc8ecb9198dd8bd3c006cbb2e73
Etanjo <ejoma05@gmail.com>
2024-03-15 12:24:11 -0400

    added the diseases

--- src/diseaseModel.ts
+++ src/diseaseModel.ts
@@ -1,5 +1,6 @@
 import type { Patient } from "./types";
 import type { SimulationParameters } from "./types";
+import { Disease, defaultSimulationParameters, diseases } from "./types";
 
 export const createPopulation = (size = 1600) => {
   const population: Patient[] = [];
@@ -18,8 +19,12 @@ export const createPopulation = (size = 1600) => {
   return population;
 };
 
+const ebola = new Disease("Ebola", 50, 44, 10);
+const covid = new Disease("Covid-19", 63, 1, 10);
+const flu = new Disease("Influenza", 38, 3, 14);
 
-
+defaultSimulationParameters.infectionChance = diseases[0].infectionChance;
+defaultSimulationParameters.deathChance = diseases[0].deathChance;
 
 const updatePatient = (
   patient: Patient,
@@ -30,9 +35,9 @@ const updatePatient = (
   // IF we are NOT sick, see if our neighbors are sick...
   // choose a partner
   const partner = population[Math.floor(Math.random() * population.length)];
-  if (partner.infected && 100*Math.random() < params.infectionChance) {          
-    updatedPatient = { ...patient, infected : true };
-  }   
+  if (partner.infected && 100 * Math.random() < params.infectionChance) {
+    updatedPatient = { ...patient, infected: true };
+  }
   return updatedPatient;
 };
 
--- src/types.ts
+++ src/types.ts
@@ -16,13 +16,23 @@ export const defaultSimulationParameters: SimulationParameters = {
   distanceThreshold: 5,
   movement: 5,
   infectionChance: 5,
+  deathChance: 0,
 };
 
-export type Disease = {
+export class Disease  {
   name:string;
   infectionChance: number;
   deathChance: number;
   recoveryTime: number;
+  constructor(n:string,i:number,d:number,r:number){
+    this.name = n
+    this. infectionChance = i
+    this.recoveryTime = r
+    this.deathChance = d
+    //@ts-ignore
+    diseases.push(this)
+
+  }
 }
 
 export let diseases = []
